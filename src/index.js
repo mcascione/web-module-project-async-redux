0e5c774a2638d0ee6bb76dfc327acd46c2cbaaef
Miranda Cascione <miranda.cascione@gmail.com>
2023-04-14 13:31:08 -0700

    mvp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import App from "./App";
-import reducer from "./reducers/index.js";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './App';
+import reducer from './reducers/index.js';
+import './index.css';
+
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
 );
